dae440bc23ad2980c73ed133a8df7fc163cdd5c2
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-07-11 20:26:36 +0200

    Switch to PySide2

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,15 +1,12 @@
+from fbs_runtime.application_context.PySide2 import ApplicationContext
+from PySide2.QtWidgets import QMainWindow
+
 import sys
 
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
-
-
-if __name__ == "__main__":
-    context = ApplicationContext()
-
+if __name__ == '__main__':
+    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
     window = QMainWindow()
     window.resize(250, 150)
     window.show()
-
-    exit_code = context.app.exec_()
+    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
